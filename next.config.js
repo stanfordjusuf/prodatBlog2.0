73240f55afa4da1473d278ae481704e18b04ee7f
stanfordjusuf <107454100+stanfordjusuf@users.noreply.github.com>
2023-01-29 20:22:38 +0700

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ['ik.imagekit.io', 'res.cloudinary.com']
+  },
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en'
+  },
+  output: 'standalone'
+};
